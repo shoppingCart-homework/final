101193d25bc15190916c2671d18cfe1f17af4de8
HinaM <55233191+HinaM@users.noreply.github.com>
2021-12-01 16:49:50 +0800

    firebase apiKey

---
+++ cart/src/settings/firebaseConfig.js
@@ -0,0 +1,17 @@
+export const config = {
+
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+
+    authDomain: "product-a87b2.firebaseapp.com",
+
+    databaseURL: "https://product-a87b2.firebaseio.com",
+
+    projectId: "product-a87b2",
+
+    storageBucket: "product-a87b2.appspot.com",
+
+    messagingSenderId: "576564577770",
+
+    appId: "1:576564577770:web:a6e3f73c173f1782dd5e93"
+
+  };
