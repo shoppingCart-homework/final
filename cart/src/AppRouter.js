cdbde01744ace3cecf300d1ca6457902290ed1f6
王渝絜 <bebo20001016@gmail.com>
2021-12-04 15:21:05 +0800

    Login／Logout

--- cart/src/AppRouter.js
+++ cart/src/AppRouter.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ProductList from './product/ProductList';
 import EmployeeList from './employee/EmployeeList';
+import Newpage from './newpage/newpage';
 import Main from './ui/Main';
 import {AuthContext, STATUS} from './account/AuthContext';
 
@@ -14,6 +15,7 @@ return (
         <Route path="/" element={<Main/>}/>
         <Route path="/product" element={<ProductList/>}/>
         <Route path="/employee" element={<EmployeeList/>}/>
+        <Route path="/newpage" element={<Newpage/>}/>
       </Routes>
     </Router>
   </AuthContext.Provider>
