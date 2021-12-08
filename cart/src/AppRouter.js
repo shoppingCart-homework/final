80da8f01d21877895e818dc00dd9ccc72d8f0e29
HinaM <55233191+HinaM@users.noreply.github.com>
2021-12-08 12:18:34 +0800

    p

--- cart/src/AppRouter.js
+++ cart/src/AppRouter.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import ProductList from './product/ProductList';
 import EmployeeList from './employee/EmployeeList';
 import Newpage from './newpage/newpage';
