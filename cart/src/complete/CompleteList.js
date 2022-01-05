43dd48b138cfe75a16301034ddb203ef7dd2a700
HinaM <55233191+HinaM@users.noreply.github.com>
2022-01-05 11:17:15 +0800

    訂單明細完成

--- cart/src/complete/CompleteList.js
+++ cart/src/complete/CompleteList.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { Box, List, ListItem, ListItemText} from '@mui/material';
+import React, {useState,useEffect} from 'react';
+import { Box, List, ListItem, ListItemText,CircularProgress} from '@mui/material';
 import AppMenu from '../ui/AppMenu';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -14,22 +14,57 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {collectionGroup, getFirestore,collection ,getDocs,doc,setDoc,addDoc, onSnapshot,query, orderBy,where,deleteDoc,updateDoc,limit,getDoc} from '@firebase/firestore';
+import { initializeApp } from "firebase/app";
+import {config} from '../settings/firebaseConfig';
+import Swal from 'sweetalert2';
 
 export default function Newpage() {
-   
-  const [employees] = useState([
-
-    {id:"0", name:"akabareiji@yahoo.com.tw", department:"天王星"},
-    {id:"1", name:"123@gmail.com", department:"海王星"}
-   ]);
-  const [buy]=useState([
-    {stuff:"微笑世界",quantity:"5"},
-    {stuff:"微笑藥水",quantity:"10"},
-    {stuff:"微笑宇宙",quantity:"15"}
-  ]);
+  const firebaseApp = initializeApp(config);
+  const db = getFirestore();
+  const [products,setProducts]=useState([]);
+  const [string,setstring]=useState([]);
+  const [productsr,setProductsr]=useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+      useEffect(()=>{
+        async function readData() {
+          setIsLoading(true);
+          var citiesRef = collection(db, "order");
+          const q = await getDocs(query(citiesRef, where("state", "==", 1)));
+          //temp裝未完成訂單user
+          const temp = [];
+          //tempu裝未完成訂單id
+          const tempu=[];
+         //q.foreach(doc) => ........
+          q.forEach(async (doc) => {
+            //訂購人email地址等資訊
+            console.log(doc.id, " => ", doc.data());
+            tempu.push("order/"+doc.id+"/content");
+            //console.log("order/"+doc.id+"/content");
+            let strr="order/"+doc.id+"/content";
+            const citiesRefi = collection(db, strr);
+            const querySnapshoti = await getDocs(citiesRefi);
+            const tempudetail=[];
+            querySnapshoti.forEach((docs) => {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(docs.id, " => ", docs.data());
+              tempudetail.push({id:docs.id,bfname:docs.data().bfname,bfprice:docs.data().bfprice, bfquantity:docs.data().bfquantity});
+              });
+              //setProductsr([...tempudetail]);
+            temp.push({id:doc.id,address:doc.data().address,email:doc.data().email, state:doc.data().state,productsr:tempudetail});
+            setProducts((current)=>[...current,{id:doc.id,address:doc.data().address,email:doc.data().email, state:doc.data().state,productsr:tempudetail} ])
+          });
+          console.log("未完成id:"+tempu);
+          console.log(temp);
+          //setProducts([...temp]);
+          setIsLoading(false);
+          setstring([...tempu]);
+        }
+        readData();
+      },[db]);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [open, setOpen] = React.useState(false);
-
+  const[productid,setProductid]=useState(0);
   const handleListItemClick = (index) => {  
     setSelectedIndex(index);
   };
@@ -39,43 +74,46 @@ export default function Newpage() {
   const handleClose = () => {
     setOpen(false);
   };
-  
-  
-
-    return (
-      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <AppMenu/>
-    <List aria-label="employee list"></List>
-    {employees.map((employee, index) => 
-    <Accordion>
+  const Imfor=function(){
+    return(
+      <Box>
+        {products.map((product, index) => 
+        <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id={index}
         >
-          <Typography>{employee.name}</Typography>
+          <Typography>{product.email}</Typography>
         </AccordionSummary>
-        
         <AccordionDetails>
-        <strong>地址：{employee.department}</strong>
+          <strong>地址：{product.address}</strong>
           <Typography>
-          {buy.map((buyy, index) => 
-            
-            <ListItemText primary={"品項："+buyy.stuff} secondary={"份數："+buyy.quantity}></ListItemText>
+          {product.productsr.map((buyy, index) => 
+            <ListItemText primary={"品項："+buyy.bfname} secondary={"份數："+buyy.bfquantity}></ListItemText>
+           
           )}
           
-          <Button variant="contained" color="success" edge="end" disabled>已完成</Button>
+          <Button variant="contained" color="success" edge="end" disabled>訂單已完成</Button>
           </Typography>
         </AccordionDetails>
       </Accordion>
-    )}
-    
+      
+        )}
+      </Box>
+    );
+  }
+  
 
-
+    return (
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <AppMenu/>
+        <List aria-label="employee list"></List>
+        {!isLoading ?
+          <Imfor/>
+           :
+          <CircularProgress />
+        }
     </Box>
-    
-
-
   );
-
 }
--- cart/src/newpage/newpage.js
+++ cart/src/newpage/newpage.js
@@ -1,5 +1,5 @@
 import React, {useState,useEffect} from 'react';
-import { Box, List, ListItem, ListItemText} from '@mui/material';
+import { Box, List, ListItem, ListItemText,CircularProgress} from '@mui/material';
 import AppMenu from '../ui/AppMenu';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -17,96 +17,70 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {collectionGroup, getFirestore,collection ,getDocs,doc,setDoc,addDoc, onSnapshot,query, orderBy,where,deleteDoc,updateDoc,limit,getDoc} from '@firebase/firestore';
 import { initializeApp } from "firebase/app";
 import {config} from '../settings/firebaseConfig';
-
+import Swal from 'sweetalert2';
 export default function Newpage() {
   const firebaseApp = initializeApp(config);
   const db = getFirestore();
   const [products,setProducts]=useState([]);
-  const [productsrrr,setProductsrrr]=useState([]);
-  const [strr,setstrr]=useState([]);
+  const [string,setstring]=useState([]);
   const [productsr,setProductsr]=useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [DocId, setDocId] = useState(0);
       useEffect(()=>{
         async function readData() {
-          
           setIsLoading(true);
           var citiesRef = collection(db, "order");
           const q = await getDocs(query(citiesRef, where("state", "==", 0)));
-    
+          //temp裝未完成訂單user
           const temp = [];
+          //tempu裝未完成訂單id
+          const tempu=[];
          //q.foreach(doc) => ........
-          q.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          
+          q.forEach(async (doc) => {
+            //訂購人email地址等資訊
             console.log(doc.id, " => ", doc.data());
-            temp.push({id:doc.id,address:doc.data().address,email:doc.data().email, state:doc.data().state});
-          });
-          console.log(temp);
-          setProducts([...temp]);
-          setIsLoading(false);
-        }
-        readData();
-      },[db]);
-      useEffect(()=>{
-
-        async function readData() {
-          
-          setIsLoading(true);
-          
-          //var string='order/5/content';
-          var string='order';
-          const citiesRef = collection(db, string);
-          
-          const querySnapshot = await getDocs(citiesRef);
-          const temp = [];
-          const str=[];
-          //q.foreach(doc) => ........
-          querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            temp.push({id:doc.id});
-            var stringr='order/'+doc.id+'/content';
-            str.push(stringr);
-            });
-          //console.log("哈哈:"+str);
-          console.log(temp);
-          setstrr([...str]);
-          setProductsr([...temp]);
-          setIsLoading(false);
-          let e=str.length;
-          const tempi = [];
-          const count=[];
-          const counti=[];
-          for (var i=0;i<e;i++){
-            //console.log("我是:"+str[i]);
-            var stringi=str[i];
-            const citiesRefi = collection(db, stringi);
+            tempu.push("order/"+doc.id+"/content");
+            //console.log("order/"+doc.id+"/content");
+            let strr="order/"+doc.id+"/content";
+            const citiesRefi = collection(db, strr);
             const querySnapshoti = await getDocs(citiesRefi);
-            //q.foreach(doc) => ........
+            const tempudetail=[];
             querySnapshoti.forEach((docs) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(docs.id, " => ", docs.data());
-            tempi.push({id:docs.id,bfname:docs.data().bfname,bfprice:docs.data().bfprice, bfquantity:docs.data().bfquantity});
-            });
-            
-            count.push(tempi.length);
-            console.log("數量"+count);
-          }
-          for (var j=0;j<count.length-1;j++){
-            counti.push(count[j+1]-count[j]);
-          }
-          console.log("長度:"+counti);
-          setProductsrrr([...tempi]);
+              // doc.data() is never undefined for query doc snapshots
+              console.log(docs.id, " => ", docs.data());
+              tempudetail.push({id:docs.id,bfname:docs.data().bfname,bfprice:docs.data().bfprice, bfquantity:docs.data().bfquantity});
+              });
+              //setProductsr([...tempudetail]);
+            temp.push({id:doc.id,address:doc.data().address,email:doc.data().email, state:doc.data().state,productsr:tempudetail});
+            setProducts((current)=>[...current,{id:doc.id,address:doc.data().address,email:doc.data().email, state:doc.data().state,productsr:tempudetail} ])
+          });
+          console.log("未完成id:"+tempu);
+          console.log(temp);
+          //setProducts([...temp]);
+          setIsLoading(false);
+          setstring([...tempu]);
         }
-        
         readData();
       },[db]);
-      
-  
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [open, setOpen] = React.useState(false);
-
+  const edit = async function(id){
+    try{
+      console.log(id);
+      await updateDoc(doc(db,"order",id),{
+        state:1
+      });
+      Swal.fire({
+        icon: 'success',
+        title: '此訂單完成'
+      })
+    }
+  
+    catch(e){
+      console.log(e);
+    }
+    
+  }
+  const[productid,setProductid]=useState(0);
   const handleListItemClick = (index) => {  
     setSelectedIndex(index);
   };
@@ -116,16 +90,10 @@ export default function Newpage() {
   const handleClose = () => {
     setOpen(false);
   };
-  for (var i=1;i<=10;i++){
-    
-  }
-  
-
-    return (
-      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <AppMenu/>
-    <List aria-label="employee list"></List>
-    {products.map((product, index) => 
+  const Imfor=function(){
+    return(
+      <Box>
+        {products.map((product, index) => 
         <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
@@ -137,24 +105,31 @@ export default function Newpage() {
         <AccordionDetails>
           <strong>地址：{product.address}</strong>
           <Typography>
-          {productsrrr.map((buyy, index) => 
+          {product.productsr.map((buyy, index) => 
             <ListItemText primary={"品項："+buyy.bfname} secondary={"份數："+buyy.bfquantity}></ListItemText>
            
           )}
           
-          <Button variant="contained" color="success" edge="end">完成訂單</Button>
+          <Button variant="contained" color="success" edge="end" onClick={()=>edit(product.id)}>完成訂單</Button>
           </Typography>
         </AccordionDetails>
       </Accordion>
+      
         )}
-    
-    
+      </Box>
+    );
+  }
+  
 
-
+    return (
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <AppMenu/>
+        <List aria-label="employee list"></List>
+        {!isLoading ?
+          <Imfor/>
+           :
+          <CircularProgress />
+        }
     </Box>
-    
-
-
   );
-
 }
